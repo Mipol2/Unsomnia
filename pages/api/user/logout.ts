d28172e58bb21eb168352defd7d25b3c6165169c
Fatih20 <fnri39@protonmail.com>
2022-08-10 11:50:26 +0700

    Attempts at debugging logout.

--- pages/api/user/logout.ts
+++ pages/api/user/logout.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest } from "next";
-import { NextApiResponseWithLocals } from "../../../types/types";
-import {createRouter} from "next-connect";
+import { createRouter } from "next-connect";
 import { logout } from "../../../controller/user.controller";
-import loginStatus from "../../../middleware/loginStatus";
 import extractJWT from "../../../middleware/extractJWT";
+import loginStatus from "../../../middleware/loginStatus";
+import { NextApiResponseWithLocals } from "../../../types/types";
 
-const router = createRouter<NextApiRequest, NextApiResponseWithLocals>()
+const router = createRouter<NextApiRequest, NextApiResponseWithLocals>();
+
 router.use(extractJWT).use(loginStatus(true)).post(logout);
 
 export default router.handler();
